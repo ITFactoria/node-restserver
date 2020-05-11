c1d47427e2d36ab39dec85343d75677dfb077985
ninojairo <itfactoria.site@gmail.com>
2020-05-11 14:14:57 -0500

    CRUD Operations

--- server/controllers/usuario.js
+++ server/controllers/usuario.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
 const app = express()
 const Usuario = require('../models/usuario');
+const usuario = require('../models/usuario');
+
 
 
 
 //GET
 app.get('/usuarios', (req, res) => {
-    res.json('GET usuario')
+
+    let from = Number(req.query.from || 0);
+    let limit = Number(req.query.limit || 5);
+
+    Usuario.find({})
+        .skip(from)
+        .limit(limit)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+            res.json({
+                ok: true,
+                usuarios
+            })
+
+        })
+    //res.json('GET usuario')
 })
 
 //POST
@@ -14,18 +39,20 @@ app.post('/usuario', (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
-        nombre : body.nombre,
-        email : body.email,
-        password : body.password,
-        img : body.img,
-        role : body.role,
-        estado : body.estado,
-        google : body.google
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        img: body.img,
+        role: body.role,
+        estado: body.estado,
+        google: body.google
 
     })
 
-    usuario.save((err, usuarioDB)=>{
-        if(err){
+
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
             return res.status(400).json({
                 ok: false,
                 err
@@ -33,7 +60,7 @@ app.post('/usuario', (req, res) => {
         }
         res.json({
             ok: true,
-            usuario : usuarioDB
+            usuario: usuarioDB
         })
     })
 })
@@ -41,13 +68,46 @@ app.post('/usuario', (req, res) => {
 //PUT
 app.put('/usuario/:id', (req, res) => {
     let id = req.params.id;
-    res.json(`Update usuario ${id}`)
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+
+    })
 })
 
 //DELETE
 app.delete('/usuario/:id', (req, res) => {
     let id = req.params.id
-    res.json(`Delete usuario ${id}`)
+
+    Usuario.findByIdAndDelete(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        if(!usuarioBorrado){
+            return res.status(400).json({
+                ok: false,
+                err : "Usuario no existe"
+            })
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    })
 })
 
 module.exports = app;
--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos ={
+    values : ['ADMIN_ROLE', 'USER_ROLE'],
+    message : '{VALUE} no es un rol valido'
+
+}
+
+
 let Schema = mongoose.Schema;
 
 //const { Schema } = require("mongoose");
@@ -10,7 +19,8 @@ let usuarioSchema = new Schema({
     },
     email :{
         type : String,
-        required : [true, 'El correo es requerido']
+        required : [true, 'El correo es requerido'],
+        unique : true
     },
     password :{
         type : String,
@@ -22,7 +32,8 @@ let usuarioSchema = new Schema({
     },
     role :{
         type : String,
-        default : 'USER_ROLE'
+        default : 'USER_ROLE',
+        enum : rolesValidos
     },
     estado :{
         type : Boolean,
@@ -35,4 +46,13 @@ let usuarioSchema = new Schema({
 
 })
 
+usuarioSchema.methods.toJSON = function(){
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+
+}
+
+usuarioSchema.plugin(uniqueValidator, {message : '{PATH} debe ser unico'})
 module.exports = mongoose.model('Usuario', usuarioSchema);
